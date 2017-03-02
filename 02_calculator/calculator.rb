fa14a8dc7cec3dc8dcc137a85cef54681ae2df82
Adam Taufmann <ataufmann@gmail.com>
2017-03-01 15:40:16 -1000

    Completed Task 02_Calculator < 10mins   - Going to start the bonus tasks

--- 02_calculator/calculator.rb
+++ 02_calculator/calculator.rb
@@ -1,1 +1,15 @@
-#write your code here
+def add(num1, num2)
+	num1+num2
+end
+
+def subtract(num1, num2)
+	num1-num2
+end
+
+def sum(array)
+	total = 0
+	array.each do |number|
+		total += number
+	end
+	total
+end
