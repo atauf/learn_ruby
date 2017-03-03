31cfd6d1464851e39c361176df6d0bbff8c598aa
Adam Taufmann <ataufmann@gmail.com>
2017-03-02 16:06:42 -1000

    Completed Multiply and Power functions < 5mins   - My equation for the Factorial challenge isn't correct, but moving on   - Got the gist of using if/elsif/else and while loop in Ruby (basically same as other languages)

--- 02_calculator/calculator.rb
+++ 02_calculator/calculator.rb
@@ -13,3 +13,31 @@ def sum(array)
 	end
 	total
 end
+
+def multiply(num1, num2)
+	num1*num2
+end
+
+def power(num1, num2)
+	return num1**num2
+end
+
+
+def factorial(num)
+	answer = 1
+	if num == 0
+		return answer
+	elsif num == 1
+		return answer
+	elsif num < 0
+		return "Not possible"
+	else
+		answer = 0
+		#this is not the correct equation for calculating a factorial
+		while num > 1
+			answer += num * (num-1) 
+			num = num-1
+		end
+	end
+	return answer
+end
